f7b3f9f093293bacb98cc89e2c18292ecc1503e7
keel-210 <crafters_touhou@yahoo.co.jp>
2018-06-05 18:30:33 +0900

    1

--- IFFT_Test.py
+++ IFFT_Test.py
@@ -7,60 +7,73 @@ import matplotlib.pyplot as plt
 import scipy.io.wavfile as wav
 from scipy.fftpack import fft, ifft
 
+np.seterr(divide='ignore', invalid='ignore')
+
+
 def _pow_scale(fft, p):
     return np.power(np.abs(fft), p) * np.sign(fft)
 
-def overwrap(fft, length, dif, side):
-    X = np.zeros((128,254))
-    for i in range(128):
-        for j in range(254):
-            if(j < 128):
-                X[i,j] = fft[0,i,j]
-            else :
-                X[i,j] = fft[0,i,254-j]
-    dst = np.zeros(dif * (side-1)+length, dtype=float)
-    for i, f in enumerate(X):
-        dst[i*dif:i*dif+length] += np.fft.ifft(f).real
+
+def overwrap(fft, length, size, stride, phase):
+    X = np.zeros((size, length))
+    for i in range(size):
+        for j in range(length):
+            if(j <= (int)(length/2)):
+                X[i, j] = fft[i, j]
+            else:
+                X[i, j] = fft[i, length-j]
+    dst = np.zeros(((length - stride) + size * stride), dtype=float)
+    for i, (f, p) in enumerate(zip(X, phase)):
+        comp_size = np.sqrt(np.power(f.real, 2)+np.power(f.imag, 2))
+        comp = (comp_size * np.cos(p)) + (1j * comp_size * np.sin(p))
+        dst[i * stride:i * stride + length] += np.fft.ifft(comp).real
+        #dst[i * stride:i * stride + length] += np.fft.ifft(f).real
     return dst
 
-def FFT(wav,stride,window,pos):
-    wave_len = stride*2 - 2
-    data_fft = np.array([wav[p:p+wave_len,0]*window for p in range(pos, pos+(wave_len-stride)+stride*stride, stride)])
-    fft_phase = np.array([np.arctan(data_fft[i].imag/data_fft[i].real) for i in range(len(data_fft))])
+
+def FFT(wav, length, stride, window, pos, size):
+    wave_len = length
+    data_fft = np.array([wav[p:p+wave_len, 0]*window for p in range(pos,
+                                                                    pos + (wave_len - stride) + size * stride, stride)])
     data_fft = np.fft.fft(data_fft, axis=1)
-    data_fft = data_fft[:,:stride]
-    data_fft = np.abs([data_fft], dtype=np.float32)
+    fft_phase = [np.arctan(f.imag/f.real) for f in data_fft]
+    data_fft = data_fft[:, :size]
+    data_fft = np.abs([data_fft], dtype='f4')
 
-    im = data_fft
-    #plt.imshow(np.squeeze(im*256))
-    #plt.waitforbuttonpress(0)
+    # im = data_fft
+    # plt.imshow(np.squeeze(im*256))
+    # plt.waitforbuttonpress(0)
 
-    return data_fft,fft_phase
+    return data_fft, fft_phase
+
 
 parser = argparse.ArgumentParser(description='')
-parser.add_argument('--path',          type=str,   default='./test.wav')
-parser.add_argument('--out_path',      type=str,   default='./out_test.wav')
-parser.add_argument('--length',        type=int,   default=254)
-parser.add_argument('--stride',        type=int,   default=128)
+parser.add_argument('--path', type=str,   default='./test.wav')
+parser.add_argument('--out_path', type=str, default='./out_test_32.wav')
+parser.add_argument('--length', type=int, default=254)  # 254固定
+parser.add_argument('--size', type=int, default=128)  # 128固定
+parser.add_argument('--stride', type=int,   default=32)
 args = parser.parse_args()
 
 bps = 141000
 stride = args.stride
-wave_len = stride*2-2
+wave_len = args.length
+size = args.size
 window = np.hanning(wave_len)
-test_len = 10
-data_size = 10000
+all_size = ((wave_len - stride) + size * stride)
 
 wav_bps, data = wav.read(args.path)
 print(wav_bps)
 
-datas = np.zeros((100,16510))
+datas = np.zeros((100, ((args.length - stride) + size * stride)))
+
 for i in range(100):
-    size, phase = FFT(data,128,window,i*((args.length-stride)+stride*stride))
-    data_ifft = overwrap(size,254,128,128)
+    fft_abs, phase = FFT(data, wave_len, stride, window, i * all_size, size)
+    data_ifft = overwrap(fft_abs[0], wave_len, size, stride, phase)
     data_ifft = np.reshape(data_ifft, -1)
     datas[i] = data_ifft
 
 datas = np.reshape(datas, -1)
-print(np.shape(datas))
-wav.write(args.out_path, (int)(wav_bps/16), datas)
+datas = np.array(datas, np.int16)
+
+wav.write(args.out_path, wav_bps, datas)
