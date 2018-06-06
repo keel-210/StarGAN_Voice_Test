6cb0d9ce3991a1f6d0eb83e5810d57ad79686305
keel-210 <crafters_touhou@yahoo.co.jp>
2018-06-07 01:17:19 +0900

    Phase attach

--- phase_Test.py
+++ phase_Test.py
@@ -24,7 +24,8 @@ def overwrap(fft, length, size, stride, phase):
                 X[i, j] = fft[i, length-j]
     dst = np.zeros(((length - stride) + size * stride), dtype=float)
     for i, (f, p) in enumerate(zip(X, phase)):
-        comp = (f.real * np.cos(p)) + (1j * f.real * np.sin(p))
+        comp_size = np.sqrt(np.power(f.real, 2)+np.power(f.imag, 2))
+        comp = (comp_size * np.cos(p)) + (1j * comp_size * np.sin(p))
         dst[i * stride:i * stride + length] += np.fft.ifft(comp).real
         #dst[i * stride:i * stride + length] += np.fft.ifft(f).real
     return dst
@@ -35,7 +36,7 @@ def FFT(wav, length, stride, window, pos, size):
     data_fft = np.array([wav[p:p+wave_len, 0]*window for p in range(pos,
                                                                     pos + (wave_len - stride) + size * stride, stride)])
     data_fft = np.fft.fft(data_fft, axis=1)
-    fft_phase = np.angle(data_fft)
+    fft_phase = [np.arctan(f.imag/f.real) for f in data_fft]
     data_fft = data_fft[:, :size]
     data_fft = np.abs([data_fft], dtype='f4')
 
@@ -65,14 +66,27 @@ print(wav_bps)
 
 datas = np.zeros((100, ((args.length - stride) + size * stride)))
 
-for i in range(100):
-    fft_abs, phase = FFT(data, wave_len, stride, window, i * all_size, size)
+print(all_size)
 
-    data_ifft = overwrap(fft_abs[0], wave_len, size, stride, phase)
-    data_ifft = np.reshape(data_ifft, -1)
-    datas[i] = data_ifft
+pos = 5000
+wav_data = data[pos:pos+wave_len,0] * window
+fft_data = np.fft.fft(wav_data)
+fft_abs = np.abs(fft_data)
+fft_phase = np.arctan2(fft_data.imag,fft_data.real)
+print(fft_phase)
+fft_comp = np.zeros(wave_len,dtype = 'complex128')
+for i in range(wave_len):
+	fft_comp[i] = fft_abs[i].real * np.cos(fft_phase[i]) + fft_abs[i].real *np.sin(fft_phase[i]) * 1j
 
-datas = np.reshape(datas, -1)
-datas = np.array(datas, np.int16)
+fig, plots  = plt.subplots(nrows=3,ncols=2, figsize=(10,5))
+plots[0,0].plot(fft_data.real,color = 'r',linewidth = 0.5)
+plots[0,1].plot(fft_data.imag,color = 'b',linewidth = 0.5)
 
-wav.write(args.out_path, wav_bps, datas)
+plots[1,0].plot(fft_abs.real,color = 'g',linewidth = 0.5)
+plots[1,1].plot(fft_abs.imag,color = 'y',linewidth = 0.5)
+
+plots[2,0].plot(fft_comp.real,color = 'g',linewidth = 0.5)
+plots[2,1].plot(fft_comp.imag,color = 'y',linewidth = 0.5)
+
+fig.show()
+plt.waitforbuttonpress()
