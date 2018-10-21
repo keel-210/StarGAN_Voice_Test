15cf4d0a0a354028e157ad33e18c3152fc45a54a
keel-210 <crafters_touhou@yahoo.co.jp>
2018-10-21 14:09:46 +0900

    MlCeps_Vocode2

--- cepstrum_test.py
+++ cepstrum_test.py
@@ -10,69 +10,47 @@ import pysptk
 
 np.seterr(divide='ignore', invalid='ignore')
 
-
-def _pow_scale(fft, p):
-    return np.power(fft, p)
-
-
-def overwrap(fft, length, size, stride, phase, scale):
-    X = np.zeros((size, length))
-    for i in range(size):
-        for j in range(length):
-            if(j <= (int)(length/2)):
-                X[i, j] = fft[i, j]
-            else:
-                X[i, j] = fft[i, length - j]
-    X /= scale
-    X = _pow_scale(X, 5)
-    dst = np.zeros(((length - stride) + size * stride), dtype=float)
-    for i, (f, p) in enumerate(zip(X, phase)):
-        comp = np.zeros(wave_len, dtype='complex128')
-        comp = (f.real * np.cos(p)) + (1j * f.real * np.sin(p))
-        dst[i * stride:i * stride + length] += np.fft.ifft(comp).real
-    return dst
-
-
-def FFT(wav, length, stride, window, pos, size):
-    data_fft = np.array(wav[pos:pos+(wave_len-size)+size*stride]*window)
-    data_fft = np.fft.fft(data_fft)
-    fft_phase = np.arctan2(data_fft.imag, data_fft.real)
-    data_fft = np.abs([data_fft], dtype='f4')
-
-    return data_fft, fft_phase
-
-
 stride = 128
 wave_len = 254
 size = 128
 all_size = ((wave_len - stride) + size * stride)
-power = 0.2
-scale = 1/18
 path = './test.wav'
 out_path = './cepstrum_test.wav'
-
+ideal_path = './cepstrum_test_ideal.wav'
+# read .wav
 wav_bps, data = wav.read(path)
-window = np.hanning(wav_bps*1)
 
-datas = np.zeros((100, ((wave_len - stride) + size * stride)))
+# const.
+length = 2 ** 18
+window = np.hanning(length)
+hopSize = 10
+pos = 1 * wav_bps
+alpha = 0.43
+order = 128
 
-pos = 4 * all_size
-data_raw = np.array(data[pos:pos+wav_bps*1], dtype='float32')
-data_fft = np.array(data_raw*window, dtype='float32')
-ceps = pysptk.sptk.mcep(data_fft, miniter=2, order=128, etype=2, eps=-1)
+data_raw = np.array(data[pos:pos + length], dtype='float64')
+wav.write(ideal_path, wav_bps, data_raw.astype('int16'))
+data_fft = np.array(data_raw*window, dtype='float64')
+ceps = pysptk.sptk.mcep(data_fft, order=order, alpha=alpha)
 ceps = np.array(ceps, dtype='float64')
-pitch = pysptk.sptk.rapt(data_raw, wav_bps, 50, otype='pitch')
-pitch = np.array(pitch, dtype='float64')
-pitch = pysptk.sptk.excite(pitch, 100)
-pitch *= 500
-#pitch = [p if p<10 else p*500 for p in pitch]
-delay = pysptk.sptk.mlsadf_delay(128,4)
-mlsa = np.array([pysptk.sptk.mlsadf(p, ceps, 0.42, 4, delay) for p in pitch],dtype='int16')
-plt.subplot(3,1,1)
+
+pitch = pysptk.sptk.swipe(data_raw, wav_bps, hopSize, max=5000, threshold=0.3, otype='pitch').astype('float64')
+pitch = pysptk.sptk.excite(pitch, hopsize=hopSize)
+pitch = [p if p > 2 else p/100 for p in pitch]
+
+b_ceps = np.array([pysptk.mc2b(ceps, alpha=alpha)])
+synth = pysptk.synthesis.Synthesizer(pysptk.synthesis.MLSADF(order=order, alpha=alpha), length-200)
+vocode = synth.synthesis(pitch, b_ceps)
+vocode /= 100
+
+plt.subplot(2, 2, 1)
 plt.plot(data_raw)
-plt.subplot(3,1,2)
+plt.subplot(2, 2, 2)
+plt.plot(data_raw)
+plt.subplot(2, 2, 3)
+plt.plot(vocode)
+plt.subplot(2, 2, 4)
 plt.plot(pitch)
-plt.subplot(3,1,3)
-plt.plot(mlsa)
 plt.show()
-wav.write(out_path,wav_bps,mlsa)
+
+wav.write(out_path, wav_bps, vocode.astype('int16'))
