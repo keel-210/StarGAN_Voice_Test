08a2cb2e80f571f5b05139d26bda8642bc4701b2
keel-210 <crafters_touhou@yahoo.co.jp>
2018-10-23 17:33:08 +0900

    MelCeps4starGANVer0.01

--- cepstrum_test.py
+++ cepstrum_test.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 import scipy.io.wavfile as wav
 from scipy.fftpack import fft, ifft
 import pysptk
+import pyworld as pw
 
 np.seterr(divide='ignore', invalid='ignore')
 
@@ -23,7 +24,7 @@ wav_bps, data = wav.read(path)
 # const.
 length = 2 ** 18
 window = np.hanning(length)
-hopSize = 10
+hopSize = 5
 pos = 1 * wav_bps
 alpha = 0.43
 order = 128
@@ -32,21 +33,23 @@ data_raw = np.array(data[pos:pos + length], dtype='float64')
 wav.write(ideal_path, wav_bps, data_raw.astype('int16'))
 data_fft = np.array(data_raw*window, dtype='float64')
 ceps = pysptk.sptk.mcep(data_fft, order=order, alpha=alpha)
-ceps = np.array(ceps, dtype='float64')
 
 pitch = pysptk.sptk.swipe(data_raw, wav_bps, hopSize, max=5000, threshold=0.3, otype='pitch').astype('float64')
+f0 = pysptk.sptk.swipe(data_raw, wav_bps, hopSize, max=5000, threshold=0.3, otype='f0').astype('float64')
 pitch = pysptk.sptk.excite(pitch, hopsize=hopSize)
-pitch = [p if p > 2 else p/100 for p in pitch]
+pitch = np.array([p if p > 2 else p/100 for p in pitch])
 
-b_ceps = np.array([pysptk.mc2b(ceps, alpha=alpha)])
-synth = pysptk.synthesis.Synthesizer(pysptk.synthesis.MLSADF(order=order, alpha=alpha), length-200)
-vocode = synth.synthesis(pitch, b_ceps)
-vocode /= 100
+#b_ceps = np.array(pysptk.mc2b(ceps, alpha=alpha))
+#synth = pysptk.synthesis.Synthesizer(pysptk.synthesis.MLSADF(order=order, alpha=alpha), length-200)
+#vocode = synth.synthesis(pitch, b_ceps)
+#vocode /= 100
+sp = pysptk.mc2sp(ceps,alpha,2048)
+vocode = pw.synthesize(f0, sp, pitch, wav_bps)
 
 plt.subplot(2, 2, 1)
 plt.plot(data_raw)
 plt.subplot(2, 2, 2)
-plt.plot(data_raw)
+plt.plot(f0)
 plt.subplot(2, 2, 3)
 plt.plot(vocode)
 plt.subplot(2, 2, 4)
